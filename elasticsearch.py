640b9f2c1ca2ee4d93ffb9b3b40dd91e3734a4d0
Peter Dixon-Moses <peter@searchintuition.com>
2012-02-09 17:40:25 -0500

    first commit

---
+++ elasticsearch.py
@@ -0,0 +1,200 @@
+'''
+Created on Feb 6, 2012
+
+@author: yoav
+
+Basic POC methods skeleton for elasticsearch
+
+'''
+
+import pyes # http://pypi.python.org/pypi/pyes/
+import csv
+
+
+######## configuration parameters ########
+SERVER = 'ec2-107-20-78-92.compute-1.amazonaws.com'
+PORT = '9200'
+RAW_DATA_FILE = 'artist-test.tsv'
+INDEX_NAME = 'artist'
+DOC_TYPE = 'artist-test-doc-type'
+
+# Index SETTINGS -- Used to configure analysers, etc...  (es.update_settings)
+SETTINGS = ''
+#SETTINGS = {
+#			u'analysis': {
+#				u'filter': {
+#					'title_ngrams': {
+#						'side': 'front',
+#						'max_gram': 20,
+#						'min_gram': 1,
+#						'type': 'edgeNGram' 
+#					}
+#				},
+#				u'analyzer': {
+#					'full_title': {
+#						'type': 'custom',
+#						'filter': [
+#							'standard',
+#							'lowercase',
+#							'asciifolding'
+#						],
+#						'tokenizer': 'standard'
+#					},
+#					'partial_title': {
+#						'type': 'custom',
+#						'filter': [
+#							'standard',
+#							'lowercase',
+#							'asciifolding',
+#							'title_ngrams'
+#						],
+#						'tokenizer': 'standard'
+#					}
+#				}
+#			}		
+#}
+
+
+# TODO: modify the mapping to represent the current schema
+MAPPING = { 
+			u'id' : {
+					u'store': u'yes',
+					u'type': u'string',
+			},
+			u'title': {
+					u'boost': 1.0,
+					u'type': u'multi_field',
+					u'fields': {
+							u'partial': {
+									u'type': u'string',
+									u'search_analyzer': u'full_phrase',
+									u'index_analyzer': u'partial_phrase'
+							},
+							u'title': {
+									u'type': u'string',
+									u'analyzer': u'full_phrase'
+							}
+					}
+			},
+			u'context' : {
+					u'index': u'analyzed',
+					u'store': u'yes',
+					u'type': u'string'
+			},
+			u'weight' : {
+					u'store': u'yes',
+					u'type': u'float'
+			},
+			u'disambig_number' : {
+					u'store': u'yes',
+					u'type': u'integer'
+			},
+			u'aliases' : {
+					u'index': u'analyzed',
+					u'store': u'yes',
+					u'type': u'string'
+			}
+# 'type' conflicts with internal ES 'type' field -- if you want to handle this field specially, the name needs changing
+#			u'type' : {
+#					'store': 'yes',
+#					'type': 'string'
+#			},
+
+
+
+}
+
+###########################################
+
+
+######## helper functions ########
+def deleteIndex(conn, indexName):
+	try:
+		conn.delete_index(indexName)
+		print "deleting " + indexName
+		return True
+	except:
+		return False
+
+def createIndex(conn, indexName, deleteIfExists=False):
+	if deleteIfExists:
+		deleteIndex(conn, indexName)
+	
+	print "creating " + indexName
+	conn.create_index(indexName)
+
+def createMapping(conn, indexName, docType, mapping):
+	conn.put_mapping(docType, mapping, [indexName])
+
+def updateSettings(conn, indexName, settings):
+	conn.update_settings(indexName, settings)
+
+
+def init(indexName, docType, mapping, settings):
+	conn = pyes.ES(['%s:%s' % (SERVER, PORT)]) 
+	createIndex(conn, indexName, deleteIfExists=True)
+	createMapping(conn, indexName, docType, mapping)
+	#updateSettings(conn, indexName, settings)
+	return conn
+
+
+
+##################################
+
+
+######## indexing and search functions ########
+
+
+def index(conn, csvDataFile, indexName, docType):
+	f = open(csvDataFile)
+
+	dialect = csv.Sniffer().sniff(f.read(1024))
+	f.seek(0)
+	reader = csv.DictReader(f, dialect=dialect)	
+	
+	
+	for i in reader:
+		conn.index(i, indexName, docType)
+	
+def search(conn, query):
+	#q = pyes.TermQuery("title", query)
+	q = pyes.TermQuery("title", query)
+	#q = pyes.Query()
+	print q.serialize()
+	
+	result = conn.search(query=q)
+	print result
+
+
+# TODO: implement exact match search (on entity title)
+def exactMatchSearch(conn, query):
+	q = pyes.TextQuery("title", query, 'phrase')
+	print q.serialize()
+	
+	result = conn.search_raw(query=q)
+	print result
+		
+# TODO: implement auto-complete search
+def prefixingSearch(conn, query):
+	q = pyes.TextQuery("title", query, 'phrase_prefix')
+	print q.serialize()
+	
+	result = conn.search_raw(query=q)
+	print result
+	
+def interactiveQuery(conn, queryFunc):
+	while True:
+		q = raw_input('--> ')
+		queryFunc(conn, q)
+
+###############################################
+
+
+######## main ########
+if __name__ == '__main__':
+	conn = init(INDEX_NAME, DOC_TYPE, MAPPING, SETTINGS)
+	index(conn, RAW_DATA_FILE, INDEX_NAME, DOC_TYPE )
+	conn.flush()
+	
+	interactiveQuery(conn, prefixingSearch)
+######################
---
+++ setup_artist.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+
+curl -XDELETE 'localhost:9200/artist';
+
+curl -XPUT 'localhost:9200/artist' -d '
+{
+   "settings": {
+	"analysis": {
+		"filter": {
+			"phrase_ngrams": {
+				"side": "front",
+				"max_gram": 20,
+				"min_gram": 1,
+				"type": "edgeNGram" 
+			}
+		},
+		"analyzer": {
+			"full_phrase": {
+				"type": "custom",
+				"filter": [
+					"standard",
+					"lowercase",
+					"asciifolding"
+				],
+				"tokenizer": "standard"
+			},
+			"partial_phrase": {
+				"type": "custom",
+				"filter": [
+					"standard",
+					"lowercase",
+					"asciifolding",
+					"phrase_ngrams"
+				],
+				"tokenizer": "standard"
+			}
+		}
+	}
+   },
+   "mappings": {
+	"artist": {
+		"properties": {
+			"id": {
+				"type": "string",
+				"store": "yes"
+			},
+			"title": {
+				"boost": 1.0,
+				"type": "multi_field",
+				"fields": {
+					"partial": {
+						"type": "string",
+						"search_analyzer": "full_phrase",
+						"index_analyzer": "partial_phrase"
+					},
+					"title": {
+						"type": "string",
+						"analyzer": "full_phrase"
+					}
+				}
+			},
+			"context": {
+				"index": "analyzed",
+				"store": "yes",
+				"type": "string"
+			},
+			"weight": {
+				"store": "yes",
+				"type": "float"
+			},
+			"disambig_number": {
+				"store": "yes",
+				"type": "integer"
+			},
+			"aliases": {
+				"index": "analyzed",
+				"store": "yes",
+				"type": "string"
+			}
+		}
+	}
+   }
+}'
+
